614c478da722f54f0ecf9c71c17c01ec068d9b61
K1ngDavid <david.roufe@gmail.com>
2023-11-24 20:12:39 +0100

    Ajout du package backend

---
+++ backend/data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Spécifiez le type de données pour les colonnes problématiques (remplacez 'int' par 'str' ou le type approprié)
+
+
+# Gérer les lignes problématiques en utilisant 'on_bad_lines'
+df = pd.read_csv('Classeur1.csv')
+
+print(df)
