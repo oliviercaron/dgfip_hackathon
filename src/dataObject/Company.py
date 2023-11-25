e6c5a3e2a72fd19354d0c1cbaab6b1c8027760ef
cestsiloinlesetoiles <osagliss21@gmail.com>
2023-11-25 02:46:10 +0100

    backend

---
+++ src/dataObject/Company.py
@@ -0,0 +1,8 @@
+class Business:
+    def __init__(self, name, nb_employees, revenue_figure, surface, department=16,choosen_tax='CFE'):
+        self.name = name
+        self.nb_employees = nb_employees
+        self.revenue_figure = revenue_figure
+        self.surface = surface
+        self.department = department
+        self.choosen_tax = choosen_tax
